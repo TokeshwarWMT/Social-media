331838828058ff1d881b95db15f7b3efe9a8906b
tokeshwar <tokeshwar007@gmail.com>
2022-10-31 14:41:31 +0530

    done

--- routes/likes.js
+++ routes/likes.js
@@ -6,46 +6,23 @@ router.post('/likeDislike', async (req, res) => {
     try {
         const data = req.body;
         const postId = req.body.postId;
-        const likerId = req.body.id;
+        const likerId = req.body.likerId;
         data.postId = postId;
-        const like = await Like.create(data);
         var postDetails = await Post.findById(postId);
-        if (postDetails.likes.includes(req.body.id)) {
-            await Post.findByIdAndUpdate(postId, { likes: postDetails.likes-1 })
-            return res.status(404).send('you disliked this post!!')
+        if (await Like.findOne({ likerId })) {
+            const like = await Like.deleteOne({ likerId });
+            console.log(await Like.findOne({ likerId }));
+            await Post.findByIdAndUpdate(postId, { likes: postDetails.likes - 1 })
+            return res.status(201).send('you disliked this post!!')
         } else {
-            await Post.findByIdAndUpdate(postId, { likes: postDetails.likes+1 })
+            const like = await Like.create(data);
+            await Post.findByIdAndUpdate(postId, { likes: postDetails.likes + 1 })
             return res.status(201).send('you liked this post!!')
         }
 
     } catch (error) {
-        // return res.status(500).send(error)
-        console.log(error)
+        return res.status(500).send(error)
     }
 });
-
-// router.get('/get', async (req, res) => {
-//     try {
-//         const likes = await Like.find().select('_id likes');
-//         return res.status(200).send(likes)
-//     } catch (error) {
-//         console.log(error)
-//     }
-// });
-
-// router.post('/dislike', async (req, res) => {
-//     try {
-//         const data = req.body;
-//         const postId = req.body.postId;
-//         const likerId = req.body.id;
-//         data.postId = postId;
-//         const like = await Like.create(data);
-//         var postDetails = await Post.findById(postId);
-//         await Post.findByIdAndUpdate(postId, { likes: postDetails.likes - 1 })
-//         return res.status(201).send(like)
-//     } catch (error) {
-//         return res.status(500).send(error)
-//     }
-// });
 
 module.exports = router;
